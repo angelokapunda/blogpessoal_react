33526c02aa7d8eeaf2afc9666ca3e5db0d45bf04
angelokapunda <angelokapunda@gmail.com>
2025-06-16 12:49:45 -0300

    :tada: Commit Inicial

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,33 @@
+
+function Home () {
+    
+    return (
+        <div style = {{
+            display: "flex",
+            justifyContent: "center",
+            width: "100vw"
+        }}>
+            <div>
+                <div style = {{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "80vw"
+                }}>
+                    <h2>Seja Bem Vindo!</h2>
+                    <p>Expresse aqui seus pensamentos e opiniões</p>
+                </div>
+                <div style = {{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "80vw"}}>
+                    <img src="https://i.imgur.com/VpwApCU.png" alt="Imágem da página Home"
+                    width="400px" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
