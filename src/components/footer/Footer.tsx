706bf4272de28e36a60d50834430ce9ce7ab235c
angelokapunda <angelokapunda@gmail.com>
2025-06-23 09:24:26 -0300

    :sparkles: Contrução das paginas de Login, Cadastro e foi adicionado a funcionalidade de rotas com BrowserRouter

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,9 +13,16 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/angelokapunda" target='_blank'>
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/angelokapunda" target='_blank'>
                         <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/angelocarlos.damosta" target='_blank'>
                         <FacebookLogo size={48} weight='bold' />
+                        </a>
+                        
                     </div>
                 </div>
             </div>
