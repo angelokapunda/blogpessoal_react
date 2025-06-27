90c3cadfa74e9098f6756ecb0c3271b085850924
angelokapunda <angelokapunda@gmail.com>
2025-06-27 16:14:50 -0300

    :sparkles: Construção da lógica de Cadastro e listagem de Tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
+import { useContext } from "react"
 
 function Navbar() {
+
+    const navigate = useNavigate()
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout(){
+        handleLogout()
+        alert("O usuário foi desconectado com sucesso!")
+        navigate("/")
+    }
+    
     return (
-        <>
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
             
@@ -10,14 +22,13 @@ function Navbar() {
                     
                     <div className='flex gap-4'>
                         Postagens
-                        Temas
-                        Cadastrar tema
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         Perfil
                         Sair
                     </div>
                 </div>
             </div>
-        </>
     )
 }
 
--- src/models/Tema.ts
+++ src/models/Tema.ts
@@ -3,7 +3,7 @@ import type Postagem from "./Postagem";
 
 export default interface Tema {
 
-    id : number;
+    id : number | undefined;
     descricao : string;
     postagem ?: Postagem[] | null;
     
